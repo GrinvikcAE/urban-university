6207bd65d661a72d6ae25e1819b300303fb04687
GRAE <grinvikc@gmail.com>
2024-02-22 16:36:49 +0700

    For cycle, list's elements;
    Signed-off-by: GRAE <grinvikc@gmail.com>

---
+++ 2023-09-30/main.py
@@ -0,0 +1,9 @@
+
+cars_list = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+cars_count = 0
+for car in cars_list:
+    print(f'Я езжу на автомобиле марки {car}')
+    cars_count += 10
+
+print(cars_count)
+

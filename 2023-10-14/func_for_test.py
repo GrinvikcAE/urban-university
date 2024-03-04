14c906349d21155af07d9155b810cd7d00d6f5f0
GRAE <grinvikc@gmail.com>
2024-03-04 22:24:11 +0700

    Function;
    Signed-off-by: GRAE <grinvikc@gmail.com>

---
+++ 2023-10-14/func_for_test.py
@@ -0,0 +1,7 @@
+
+def print_1() -> None:
+    print("Hello World from print_1")
+
+
+def print_2() -> None:
+    print("Hello World from print_2")
---
+++ 2023-10-14/main.py
@@ -0,0 +1,4 @@
+from func_for_test import print_1 as pr1, print_2 as pr2
+
+pr1()
+pr2()

369eefbfaf2454a06038c52abcadf0704935effc
GRAE <grinvikc@gmail.com>
2024-03-31 18:06:59 +0700

    Classes
    Signed-off-by: GRAE <grinvikc@gmail.com>

--- 2023-10-25/main.py
+++ 2023-10-25/main.py
@@ -1,10 +1,8 @@
 
 
 class House:
-    def __init__(self):
-        self.number_of_floors = 10
+    number_of_floors = 10
 
 
-house = House()
-for i in range(1, house.number_of_floors + 1):
+for i in range(1, House.number_of_floors + 1):
     print(f'Текущий этаж равен: {i}')
